56cf54b2ed6c5090de6411968d85406e92445687
Heather Anderson & Sarah Joy Calpo <student@epicodus.com>
2015-02-25 10:09:43 -0800

    Add entire Class Roster.

---
+++ controllers/StudentsController.js
@@ -0,0 +1,6 @@
+courseRoster.controller("StudentsCtrl", function StudentsCtrl($scope, $stateParams, CoursesFactory, UtilitiesFactory) {
+  $scope.course = UtilitiesFactory.findById(CoursesFactory.courses, $stateParams.courseId)
+  $scope.addStudent = function() {
+    $scope.course.students.push({ name: $scope.studentName });
+  }
+});
